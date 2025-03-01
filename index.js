8974fe33bec9a83a5901b73b84a3dee9d1ab3211
meabhay <as3720719@gmail.com>
2025-03-01 17:04:32 +0530

    updated code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
- const express = require("express");
- const port = 4000;
+ require("dotenv").config();
+ const express = require("express"); 
+ const port = process.env.PORT || 3000;
  const app = express();
 
  app.use(express.json());    //Middleware
